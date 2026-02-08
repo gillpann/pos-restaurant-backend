2218191554d956f3b140eb0dd3cde105359d48e4
gillpann <cpundong@gmail.com>
2026-02-08 12:46:15 +0700

    feat: set up global error handler

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/database");
+const config = require("./config/config");
+const globalErrorHandler = require("./middlewares/globalErrorHandler");
+const createHttpError = require("http-errors");
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = config.port;
 connectDB();
 
+// Root Endpoint
 app.get("/", (req, res) => {
     res.json({message: "Hello from POS Server!"});
 });
 
+// Global Error Handler
+app.use(globalErrorHandler);
+
+// Server
 app.listen(PORT, () => {
     console.log(`POS Server is listening on port ${PORT}`);
 })
